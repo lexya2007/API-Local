d4d99a725a813f56f8e6b5b7dbabf28d683b1254
bi <nguyen.nd9362@leftcoastlogic.com>
2019-05-17 17:06:28 +0700

    update 1

--- tests/RunAllScripts_v2.2.sh
+++ tests/RunAllScripts_v2.2.sh
@@ -32,15 +32,15 @@ environment=../Postman_environment/v2.2/environment_TestServer.json
 # echo "Environment: $environment"
 # newman run $settingaccounts_collection -e $environment --insecure
 
-# tracking_collection=../Postman_Collections_APIs/v2.2/API-Tracking.json
-# echo "Collection: $tracking_collection"
-# echo "Environment: $environment"
-# newman run $tracking_collection -e $environment --insecure
+ tracking_collection=../Postman_Collections_APIs/v2.2/API-Tracking.json
+ echo "Collection: $tracking_collection"
+ echo "Environment: $environment"
+ newman run $tracking_collection -e $environment --insecure
 
-trash_collection=../Postman_Collections_APIs/v2.2/API-Trash.json
-echo "Collection: $trash_collection"
-echo "Environment: $environment"
-newman run $trash_collection -e $environment --insecure
+#trash_collection=../Postman_Collections_APIs/v2.2/API-Trash.json
+#echo "Collection: $trash_collection"
+#echo "Environment: $environment"
+#newman run $trash_collection -e $environment --insecure
 
 
 

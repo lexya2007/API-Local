300abdd52755236ba572f5f9575f31f714814836
bi <nguyen.nd9362@leftcoastlogic.com>
2019-05-17 16:51:06 +0700

    update 1

--- tests/RunAllScripts_v2.2.sh
+++ tests/RunAllScripts_v2.2.sh
@@ -5,33 +5,6 @@ environment=../Postman_environment/v2.2/environment_TestServer.json
 
 #run script for each APIs
 
-# Tu's scripts
-# project_collection=../Postman_Collections_APIs/v2.2/API-Projects.json
-# echo "Collection: $project_collection"
-# echo "Environment: $environment"
-# newman run $project_collection -e $environment --insecure
-
-# canvas_collection=../Postman_Collections_APIs/v2.0/API-Canvas.json
-# echo "Collection: $canvas_collection"
-# echo "Environment: $environment"
-# newman run $canvas_collection -e $environment --insecure
-
-# setting_collection=../Postman_Collections_APIs/v2.0/API-Setting.json
-# echo "Collection: $setting_collection"
-# echo "Environment: $environment"
-# newman run $setting_collection -e $environment --insecure
-
-# device_collection=../Postman_Collections_APIs/v2.0/API-DeviceToken.json
-# echo "Collection: $device_collection"
-# echo "Environment: $environment"
-# newman run $device_collection -e $environment --insecure
-
-# history_collection=../Postman_Collections_APIs/v2.2/API-ContactHistory.json
-# echo "Collection: $history_collection"
-# echo "Environment: $environment"
-# newman run $history_collection -e $environment --insecure
-
-
 # Bi's scripts
 
 # users_collection=../Postman_Collections_APIs/v2.2/API-Users.json

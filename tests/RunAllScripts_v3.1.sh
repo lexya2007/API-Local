16d1ef3d982aead588bca1df05713d7ce17b120e
bi <nguyen.nd9362@leftcoastlogic.com>
2019-06-06 11:34:08 +0700

    update 06/06/2019 - 11:34 AM

--- tests/RunAllScripts_v3.1.sh
+++ tests/RunAllScripts_v3.1.sh
@@ -1,52 +1,52 @@
 #!/usr/bin/env bash
 
 #create variables
-environment=../Postman_environment/v3.1/environment_123Server.json
+environment=../Postman_environment/v3.0/environment_123Server.json
 
 #run script for each APIs
 
 # Tu's scripts
-setting_collection=../Postman_Collections_APIs/v3.0/API-Setting.json
-echo "Collection: $setting_collection"
-echo "Environment: $environment"
-newman run $setting_collection -e $environment --insecure
-
-device_collection=../Postman_Collections_APIs/v3.0/API-DeviceToken.json
-echo "Collection: $device_collection"
-echo "Environment: $environment"
-newman run $device_collection -e $environment --insecure
-
-history_collection=../Postman_Collections_APIs/v3.1/API-ContactHistory.json
-echo "Collection: $history_collection"
-echo "Environment: $environment"
-newman run $history_collection -e $environment --insecure
-
-canvas_collection=../Postman_Collections_APIs/v3.1/API-Canvas.json
-echo "Collection: $canvas_collection"
-echo "Environment: $environment"
-newman run $canvas_collection -e $environment --insecure
-
-canvas_collection_share=../Postman_Collections_APIs/v3.1/API-Canvas_share.json
-echo "Collection: $canvas_collection"
-echo "Environment: $environment"
-newman run $canvas_collection_share -e $environment --insecure
-
-project_collection=../Postman_Collections_APIs/v3.1/API-Projects.json
-echo "Collection: $project_collection"
-echo "Environment: $environment"
-newman run $project_collection -e $environment --insecure
-
-project_collection_share=../Postman_Collections_APIs/v3.1/API-Projects_share.json
-echo "Collection: $project_collection"
-echo "Environment: $environment"
-newman run $project_collection_share -e $environment --insecure
+#setting_collection=../Postman_Collections_APIs/v3.0/API-Setting.json
+#echo "Collection: $setting_collection"
+#echo "Environment: $environment"
+#newman run $setting_collection -e $environment --insecure
+#
+#device_collection=../Postman_Collections_APIs/v3.0/API-DeviceToken.json
+#echo "Collection: $device_collection"
+#echo "Environment: $environment"
+#newman run $device_collection -e $environment --insecure
+#
+#history_collection=../Postman_Collections_APIs/v3.1/API-ContactHistory.json
+#echo "Collection: $history_collection"
+#echo "Environment: $environment"
+#newman run $history_collection -e $environment --insecure
+#
+#canvas_collection=../Postman_Collections_APIs/v3.1/API-Canvas.json
+#echo "Collection: $canvas_collection"
+#echo "Environment: $environment"
+#newman run $canvas_collection -e $environment --insecure
+#
+#canvas_collection_share=../Postman_Collections_APIs/v3.1/API-Canvas_share.json
+#echo "Collection: $canvas_collection"
+#echo "Environment: $environment"
+#newman run $canvas_collection_share -e $environment --insecure
+#
+#project_collection=../Postman_Collections_APIs/v3.1/API-Projects.json
+#echo "Collection: $project_collection"
+#echo "Environment: $environment"
+#newman run $project_collection -e $environment --insecure
+#
+#project_collection_share=../Postman_Collections_APIs/v3.1/API-Projects_share.json
+#echo "Collection: $project_collection"
+#echo "Environment: $environment"
+#newman run $project_collection_share -e $environment --insecure
 
 
 # Bi's scripts
-users_collection=../Postman_Collections_APIs/v2.0/API-Users.json
-echo "Collection: $users_collection"
-echo "Environment: $environment"
-newman run $users_collection -e $environment --insecure
+#users_collection=../Postman_Collections_APIs/v2.0/API-Users.json
+#echo "Collection: $users_collection"
+#echo "Environment: $environment"
+#newman run $users_collection -e $environment --insecure
 
 file_collection=../Postman_Collections_APIs/v2.0/API-File.json
 echo "Collection: $file_collection"
